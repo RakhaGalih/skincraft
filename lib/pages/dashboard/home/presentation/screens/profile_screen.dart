17cde909133358683f81255e1930d4e9dc3e7a5a
rakha <rakhagalih19@gmail.com>
2025-04-11 09:38:57 +0700

    mengupdate bagian profil

--- lib/pages/dashboard/home/presentation/screens/profile_screen.dart
+++ lib/pages/dashboard/home/presentation/screens/profile_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:skincraft/core/components/border_card.dart';
 import 'package:skincraft/core/theme/app_colors.dart';
 import 'package:skincraft/core/theme/app_sizes.dart';
 import 'package:skincraft/core/theme/app_typography.dart';
@@ -79,6 +78,9 @@ class _ProfileScreenState extends State<ProfileScreen> {
               SizedBox(
                 height: 120,
                 child: SingleChildScrollView(
+                    padding: EdgeInsets.only(
+                      left: context.as.padding,
+                    ),
                     scrollDirection: Axis.horizontal,
                     child: Row(
                         children: List.generate(skinTypes.length, (index) {
@@ -102,7 +104,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
                                   textAlign: TextAlign.center,
                                   style: AppTypograph.label1.bold),
                             ),
-                            
                             Container(
                               width: 132,
                               padding: EdgeInsets.symmetric(
