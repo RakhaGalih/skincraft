48dfab677f3bd1b94c097a0654efa7389120ea03
rakha <rakhagalih19@gmail.com>
2025-04-11 09:00:29 +0700

    menambahkan update protil

---
+++ lib/pages/dashboard/home/models/skin_model.dart
@@ -0,0 +1,8 @@
+class SkinModel {
+  String title;
+  String value;
+  SkinModel({
+    required this.title,
+    required this.value,
+  });
+}
--- lib/pages/dashboard/home/presentation/screens/profile_screen.dart
+++ lib/pages/dashboard/home/presentation/screens/profile_screen.dart
@@ -1,11 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:skincraft/core/components/border_card.dart';
 import 'package:skincraft/core/theme/app_colors.dart';
 import 'package:skincraft/core/theme/app_sizes.dart';
 import 'package:skincraft/core/theme/app_typography.dart';
+import 'package:skincraft/pages/dashboard/home/models/skin_model.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  List<SkinModel> skinTypes = [
+    SkinModel(
+      title: 'Skin Type',
+      value: 'Combination skin',
+    ),
+    SkinModel(
+      title: 'Senstivity',
+      value: 'Sensitive',
+    ),
+    SkinModel(
+      title: 'Elasticity',
+      value: 'Somewhat elastic',
+    ),
+    SkinModel(
+      title: 'Acne Proneness',
+      value: 'Clear',
+    ),
+    SkinModel(
+      title: 'Tone',
+      value: 'Uneven tone',
+    ),
+    SkinModel(
+      title: 'Texture',
+      value: 'Smooth',
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +55,7 @@ class ProfileScreen extends StatelessWidget {
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(context.as.size30 + 16),
                   border: Border.all(
-                    color: AppColor.secondaryColor.withOpacity(0.6),
+                    color: AppColor.primaryColor.withOpacity(0.6),
                     width: 6,
                   ),
                 ),
@@ -48,47 +81,47 @@ class ProfileScreen extends StatelessWidget {
                 child: SingleChildScrollView(
                     scrollDirection: Axis.horizontal,
                     child: Row(
-                        children: List.generate(4, (index) {
+                        children: List.generate(skinTypes.length, (index) {
                       return Padding(
                         padding: const EdgeInsets.only(left: 8),
-                        child: Column(
-                          children: [
-                            Container(
-                              width: 132,
-                              padding: EdgeInsets.symmetric(
-                                vertical: context.as.padding,
-                              ),
-                              decoration: const BoxDecoration(
-                                color: AppColor.secondaryColor,
-                                borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(10),
-                                  topRight: Radius.circular(10),
+                        child: BorderCard(
+                          child: Column(
+                            children: [
+                              Container(
+                                width: 132,
+                                padding: EdgeInsets.symmetric(
+                                  vertical: context.as.padding,
                                 ),
-                              ),
-                              child: Text('Skin Type',
-                                  textAlign: TextAlign.center,
-                                  style: AppTypograph.label1.bold),
-                            ),
-                            const SizedBox(
-                              height: 4,
-                            ),
-                            Container(
-                              width: 132,
-                              padding: EdgeInsets.symmetric(
-                                vertical: context.as.padding,
-                              ),
-                              decoration: const BoxDecoration(
-                                color: AppColor.secondaryColor,
-                                borderRadius: BorderRadius.only(
-                                  bottomLeft: Radius.circular(10),
-                                  bottomRight: Radius.circular(10),
+                                decoration: const BoxDecoration(
+                                  color: AppColor.primaryColor,
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(10),
+                                    topRight: Radius.circular(10),
+                                  ),
                                 ),
+                                child: Text(skinTypes[index].title,
+                                    textAlign: TextAlign.center,
+                                    style: AppTypograph.label1.bold),
                               ),
-                              child: Text('Combination skin',
-                                  textAlign: TextAlign.center,
-                                  style: AppTypograph.label2.medium),
-                            ),
-                          ],
+                              
+                              Container(
+                                width: 132,
+                                padding: EdgeInsets.symmetric(
+                                  vertical: context.as.padding,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: AppColor.primaryColor.withOpacity(0.6),
+                                  borderRadius: const BorderRadius.only(
+                                    bottomLeft: Radius.circular(10),
+                                    bottomRight: Radius.circular(10),
+                                  ),
+                                ),
+                                child: Text(skinTypes[index].value,
+                                    textAlign: TextAlign.center,
+                                    style: AppTypograph.label2.medium),
+                              ),
+                            ],
+                          ),
                         ),
                       );
                     }))),
